4e3336aecbc024988f01abefa583bbcbbe3d68b4
aiceplays <76433309+aiceplays@users.noreply.github.com>
2021-11-28 16:22:25 +0100

    Create Blueprint.cs

---
+++ Blueprint.cs
@@ -0,0 +1,10 @@
+[System.Serializable]
+public class Blueprint 
+{
+    public string type;
+    public int level;
+    public float fuel;
+    public UnityEngine.Vector3 pos;
+    public UnityEngine.Vector3 rot;
+    public float maxT;
+}
